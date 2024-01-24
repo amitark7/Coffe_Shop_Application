ae448b6f7468298b88f5aef94709da54c452218e
amitark7 <amitkumar1600121@gmail.com>
2024-01-24 16:51:16 +0530

    Update PaymentFooter.tsx

--- src/components/PaymentFooter.tsx
+++ src/components/PaymentFooter.tsx
@@ -1,5 +1,6 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
+import { BORDERRADIUS, COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme';
 
 interface PriceProp {
   price: string;
@@ -18,12 +19,58 @@ const PaymentFooter: React.FC<PaymentFooterProp> = ({
   buttonTitle,
 }) => {
   return (
-    <View>
-      <Text>PaymentFooter</Text>
+    <View style={styles.PriceFooter}>
+      <View style={styles.PriceFooterContainer}>
+        <Text style={styles.priceTitle }>Price</Text>
+        <Text style={styles.priceText}>
+          {price.currency} <Text style={styles.Price}>{price.price}</Text>
+        </Text>
+      </View>
+      <TouchableOpacity style={styles.PayButon} onPress={()=>buttonPressHandler()}>
+        <Text style={styles.ButtonText}>{buttonTitle}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-export default PaymentFooter;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  PriceFooter:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'center',
+    gap:SPACING.space_20,
+    padding:SPACING.space_20
+  },
+  PriceFooterContainer:{
+      alignItems:'center',
+      width:100
+  },
+  priceTitle:{
+    fontFamily:FONTFAMILY.poppins_medium,
+    fontSize:FONTSIZE.size_14,
+    color:COLORS.secondaryLightGreyHex
+  },
+  priceText:{
+    fontFamily:FONTFAMILY.poppins_semibold,
+    fontSize:FONTSIZE.size_24,
+    color:COLORS.primaryOrangeHex
+  },
+  Price:{
+    color:COLORS.primaryWhiteHex
+  },
+  PayButon:{
+    backgroundColor:COLORS.primaryOrangeHex,
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+    height:SPACING.space_36*2,
+    borderRadius:BORDERRADIUS.radius_20
+  },
+  ButtonText:{
+    fontFamily:FONTFAMILY.poppins_semibold,
+    fontSize:FONTSIZE.size_18,
+    color:COLORS.primaryWhiteHex
+  }
+});
+export default PaymentFooter;
