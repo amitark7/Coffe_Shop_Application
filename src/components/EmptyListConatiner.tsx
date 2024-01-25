54b031586d537422bf3c10afcd08b48f55e89f49
amitark7 <amitkumar1600121@gmail.com>
2024-01-25 15:08:03 +0530

    Update EmptyListConatiner.tsx

--- src/components/EmptyListConatiner.tsx
+++ src/components/EmptyListConatiner.tsx
@@ -1,14 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
-const EmptyListConatiner = () => {
-  return (
-    <View>
-      <Text>EmptyListConatiner</Text>
-    </View>
-  )
+interface EmptyListConatinerProp {
+  title: string;
 }
 
-export default EmptyListConatiner
+const EmptyListConatiner: React.FC<EmptyListConatinerProp> = ({title}) => {
+  return (
+    <View style={styles.EmptyCartContainer}>
+      <Text>{title}</Text>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  EmptyCartContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+export default EmptyListConatiner;
