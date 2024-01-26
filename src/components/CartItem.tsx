380c48829372c3768c4a3091c757336fd5770d6e
amitark7 <amitkumar1600121@gmail.com>
2024-01-27 01:51:16 +0530

    Update CartItem.tsx

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,36 +1,36 @@
-import { ImageProps, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {ImageProps, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
-interface CartItemProp{
-  id:string;
+interface CartItemProp {
+  id: string;
   title: string;
-  rosted:string;
-  imagelink_square:ImageProps;
-  prices:any;
-  special_ingredient:string;
-  type:string;
-  incrementCartItemQuantityHandler:()=>{}
-  decrementCartItemQuantityHandler:()=>{}
+  rosted: string;
+  imagelink_square: ImageProps;
+  prices: any;
+  special_ingredient: string;
+  type: string;
+  incrementCartItemQuantityHandler:any;
+  decrementCartItemQuantityHandler: any;
 }
 
-const CartItem:React.FC<CartItemProp> = ({
+const CartItem: React.FC<CartItemProp> = ({
   id,
-title,
-rosted,
-imagelink_square,
-prices,
-special_ingredient,
-type,
-incrementCartItemQuantityHandler,
-decrementCartItemQuantityHandler
+  title,
+  rosted,
+  imagelink_square,
+  prices,
+  special_ingredient,
+  type,
+  incrementCartItemQuantityHandler,
+  decrementCartItemQuantityHandler,
 }) => {
   return (
     <View>
       <Text>CartItem</Text>
     </View>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
