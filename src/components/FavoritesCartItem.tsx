2fe443cd5f5d899efa726fcd2ceab4c51bcda5c5
amitark7 <amitkumar1600121@gmail.com>
2024-01-28 00:39:21 +0530

    Update FavoritesCartItem.tsx

--- src/components/FavoritesCartItem.tsx
+++ src/components/FavoritesCartItem.tsx
@@ -1,6 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+interface FavoritesCartItemProp{
+  id:string;
+  name:string;
+  type:string;
+  average_rating:string
+}
+
 const FavoritesCartItem = () => {
   return (
     <View>
