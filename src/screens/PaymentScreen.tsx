3e5dad7b1b880cdf659c2cff1b3b3215683b08f4
amitark7 <amitkumar1600121@gmail.com>
2024-01-28 01:17:32 +0530

    Update PaymentScreen.tsx

--- src/screens/PaymentScreen.tsx
+++ src/screens/PaymentScreen.tsx
@@ -1,42 +1,69 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import { COLORS } from '../theme/theme'
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
+import {COLORS, FONTSIZE} from '../theme/theme';
+import GradientBGIcon from '../components/GradientBGIcon';
 
-const PaymentList=[
+const PaymentList = [
   {
-    name:'Wallet',
-    icon:'icon',
-    isIcon:true
-  },{
-    name:'Google Pay',
-    icon:require('../assets/app_images/gpay.png'),
-    isIcon:false
-  },{
-    name:'Apple Pay',
-    icon:require('../assets/app_images/applepay.png'),
-    isIcon:false
-  },{
-    name:'Amazon Pay',
-    icon:require('../assets/app_images/amazonpay.png'),
-    isIcon:false
-  }
-]
+    name: 'Wallet',
+    icon: 'icon',
+    isIcon: true,
+  },
+  {
+    name: 'Google Pay',
+    icon: require('../assets/app_images/gpay.png'),
+    isIcon: false,
+  },
+  {
+    name: 'Apple Pay',
+    icon: require('../assets/app_images/applepay.png'),
+    isIcon: false,
+  },
+  {
+    name: 'Amazon Pay',
+    icon: require('../assets/app_images/amazonpay.png'),
+    isIcon: false,
+  },
+];
 
 const PaymentScreen = () => {
-  const[paymentMode,setPaymentMode]=useState('Credit Card')
+  const [paymentMode, setPaymentMode] = useState('Credit Card');
 
   return (
     <View style={styles.ScreenContainer}>
-      <StatusBar backgroundColor={COLORS.primaryBlackHex}/>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}>
+        <View style={styles.HeaderConatiner}>
+          <TouchableOpacity>
+            <GradientBGIcon
+              name="left"
+              color={COLORS.primaryLightGreyHex}
+              size={FONTSIZE.size_16}
+            />
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
-  ScreenContainer:{
-    flex:1,
-    backgroundColor:COLORS.primaryBlackHex
-  }
-})
-export default PaymentScreen
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  ScrollViewFlex: {
+    flexGrow: 1,
+  },
+  HeaderConatiner: {},
+});
+export default PaymentScreen;
