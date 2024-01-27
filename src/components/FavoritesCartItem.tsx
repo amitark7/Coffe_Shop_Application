b3023271e880af85206ad31705adce4e85c7c488
amitark7 <amitkumar1600121@gmail.com>
2024-01-28 00:50:08 +0530

    Update FavoritesCartItem.tsx

--- src/components/FavoritesCartItem.tsx
+++ src/components/FavoritesCartItem.tsx
@@ -1,21 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {ImageProps, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
-interface FavoritesCartItemProp{
-  id:string;
-  name:string;
-  type:string;
-  average_rating:string
+interface FavoritesCartItemProp {
+  id: string;
+  name: string;
+  rosted: string;
+  imagelink_square: ImageProps;
+  special_ingredient: string;
+  type: string;
+  average_rating: number;
+  description: string;
+  favourite: boolean;
+  rating_count: string;
+  ingredients: string;
+  ToogleFavoriteItem: any;
 }
 
-const FavoritesCartItem = () => {
+const FavoritesCartItem: React.FC<FavoritesCartItemProp> = ({
+  id,
+  name,
+  rosted,
+  imagelink_square,
+  special_ingredient,
+  type,
+  average_rating,
+  description,
+  favourite,
+  rating_count,
+  ingredients,
+  ToogleFavoriteItem,
+}) => {
   return (
     <View>
       <Text>FavoritesCartItem</Text>
     </View>
-  )
-}
+  );
+};
 
-export default FavoritesCartItem
+export default FavoritesCartItem;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
