a95f5e0dff73ca10541213995acc28b6845483bf
amitark7 <amitkumar1600121@gmail.com>
2024-01-28 00:25:45 +0530

    Update FavoritesScreen.tsx

--- src/screens/FavoritesScreen.tsx
+++ src/screens/FavoritesScreen.tsx
@@ -1,7 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useStore } from '../store/store'
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
 
-const FavoritesScreen = () => {
+const FavoritesScreen = ({navigation}:any) => {
+
+  const FavoritesList=useStore((state:any)=>state.FavoritesList)
+  const TabBarBottomHeight=useBottomTabBarHeight()
   return (
     <View>
       <Text>FavoritesScreen</Text>
