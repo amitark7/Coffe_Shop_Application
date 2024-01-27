498e4a2741f8e0fe1939a42d7288dac8ee397baf
amitark7 <amitkumar1600121@gmail.com>
2024-01-28 01:15:18 +0530

    Update PaymentScreen.tsx

--- src/screens/PaymentScreen.tsx
+++ src/screens/PaymentScreen.tsx
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StatusBar, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { COLORS } from '../theme/theme'
+
+const PaymentList=[
+  {
+    name:'Wallet',
+    icon:'icon',
+    isIcon:true
+  },{
+    name:'Google Pay',
+    icon:require('../assets/app_images/gpay.png'),
+    isIcon:false
+  },{
+    name:'Apple Pay',
+    icon:require('../assets/app_images/applepay.png'),
+    isIcon:false
+  },{
+    name:'Amazon Pay',
+    icon:require('../assets/app_images/amazonpay.png'),
+    isIcon:false
+  }
+]
 
 const PaymentScreen = () => {
+  const[paymentMode,setPaymentMode]=useState('Credit Card')
+
   return (
-    <View>
-      <Text>PaymentScreen</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex}/>
     </View>
   )
 }
 
-export default PaymentScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  ScreenContainer:{
+    flex:1,
+    backgroundColor:COLORS.primaryBlackHex
+  }
+})
+export default PaymentScreen
