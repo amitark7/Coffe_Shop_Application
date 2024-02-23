e7f2e326a52c6bc131f20ae7c9465400d4ea9289
amitark7 <136192783+amitark7@users.noreply.github.com>
2024-02-23 22:59:07 +0530

    Update OrderHistoryScreen.tsx

--- src/screens/OrderHistoryScreen.tsx
+++ src/screens/OrderHistoryScreen.tsx
@@ -21,6 +21,7 @@ import EmptyListConatiner from '../components/EmptyListConatiner';
 import PopUpAnimation from '../components/PopUpAnimation';
 import OrderHistoryCard from '../components/OrderHistoryCard';
 
+
 const OrderHistoryScreen = ({navigation}: any) => {
   const [showAnimation, setShowAnimation] = useState(false);
   const OrderHistoryList = useStore((state: any) => state.OrderHistoryList);
