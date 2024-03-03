c512b096c5f3ffd1f1d571ca271ec5bfab473257
amitark7 <136192783+amitark7@users.noreply.github.com>
2024-03-03 16:19:55 +0530

    Update OrderHistoryScreen.tsx

--- src/screens/OrderHistoryScreen.tsx
+++ src/screens/OrderHistoryScreen.tsx
@@ -24,8 +24,9 @@ import OrderHistoryCard from '../components/OrderHistoryCard';
 const OrderHistoryScreen = ({navigation}: any) => {
 
   const [showAnimation, setShowAnimation] = useState(false);
-  
+
   const OrderHistoryList = useStore((state: any) => state.OrderHistoryList);
+  
   const tabBarHeight = useBottomTabBarHeight();
   const buttonPresshandler = () => {
     setShowAnimation(true);
